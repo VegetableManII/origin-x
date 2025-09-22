264cc874da303e0ac9f8dcffa4ee25f9d24269a4
VegetableManII <1343739480@qq.com>
2025-09-22 14:15:21 +0800

    refactor(ui): 优化UI

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import { PropsWithChildren } from 'react'
-import { useLaunch } from '@tarojs/taro'
+import { useLaunch, useShareAppMessage, useShareTimeline } from '@tarojs/taro'
 import { UserProvider } from './stores/userStore'
 
 import './app.less'
@@ -8,6 +7,23 @@ import './app.less'
 function App({ children }: PropsWithChildren<any>) {
   useLaunch(() => {
     console.log('App launched.')
+  })
+
+  // 小程序分享给好友
+  useShareAppMessage(() => {
+    return {
+      title: '表情包动起来 - AI驱动动图生成',
+      path: '/pages/workspace/index',
+      imageUrl: '' // 可以设置分享图片
+    }
+  })
+
+  // 小程序分享到朋友圈
+  useShareTimeline(() => {
+    return {
+      title: '表情包动起来 - AI驱动动图生成',
+      imageUrl: '' // 可以设置分享图片
+    }
   })
 
   // children 是将要会渲染的页面
